6c16a797844f52aed5ddcf4756349100497aa6e6
emily knopf <emlbragg@Gmail.com>
2022-06-23 15:41:38 -0500

    commit tree 0b5c0cdf76efc2765c10e95b024e38d0df36132d

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution here
+function findMatching(names, string1){
+    return names.filter((match1) => match1.toLowerCase() === string1.toLowerCase()
+
+    )
+}
+
+function fuzzyMatch(names2, string2){
+    return names2.filter((nameMatch) => nameMatch.toLowerCase().indexOf(string2.toLowerCase()) === 0 
+    )
+}
+
+function matchName(obj, string3){
+    return obj.filter((nameHometown) => nameHometown.name === string3)
+    
+}
